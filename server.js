00785c9322519dfd3ba6726867421e7689859c25
Paul <paulfirmin1@gmail.com>
2020-12-12 23:31:43 +0000

    prepare for deployment

--- server.js
+++ server.js
@@ -7,11 +7,6 @@ const app = express();
 
 connectDB();
 
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build"));
-});
-
 app.use(cors());
 app.use(express.json({ extended: true }));
 
@@ -19,6 +14,11 @@ app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/tags", require("./routes/api/tags"));
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build"));
+});
 
 const PORT = process.env.PORT || 5000;
 
